7b24fb8680ed9de1a41f1c90b651b1f119e0588e
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-26 00:22:24 +0200

    Add 'use client' directive to ExportImport component

--- src/components/journal/ExportImport.tsx
+++ src/components/journal/ExportImport.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useRef } from 'react';
 import { exportJournalEntries, importJournalEntries } from '@/lib/exportImport';
 import { JournalEntry } from '@/types/journal';
