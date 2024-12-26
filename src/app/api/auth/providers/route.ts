daf6ca89573a93c85dacaf5b134033a415cd09d9
Tom <morhendos@gmail.com>
2024-12-26 20:12:57 +0100

    changes

--- src/app/api/auth/providers/route.ts
+++
@@ -1,7 +0,0 @@
-import { authConfig } from '@/lib/auth/config'
-import NextAuth from 'next-auth'
-import { NextResponse } from 'next/server'
-
-const handler = NextAuth(authConfig)
-
-export const GET = handler.providers
