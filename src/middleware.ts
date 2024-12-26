1204f2fbfe2ea04e0e26a8c3fb2a3f103a1f7db3
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-26 20:10:37 +0200

    fix: Simplify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,16 @@
-import { authConfig } from '@/lib/auth/config'
-import NextAuth from 'next-auth'
+import { auth } from '@/lib/auth/config'
+import { NextResponse } from 'next/server'
 
-export const middleware = NextAuth(authConfig).auth
+export default auth((req) => {
+  const isAuthenticated = !!req.auth
+  const isOnLoginPage = req.nextUrl.pathname === '/login'
+
+  if (isAuthenticated && isOnLoginPage) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  return null
+})
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
