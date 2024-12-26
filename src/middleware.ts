fef71f2672ebcd648ae19494b55a2434d7e578a4
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-26 21:18:54 +0200

    fix: simplify auth redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,29 @@
-export { default } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+import { withAuth } from 'next-auth/middleware'
+
+export default withAuth(
+  function middleware(req) {
+    const isAuth = !!req.nextauth.token
+    const isAuthPage = req.nextUrl.pathname.startsWith('/login')
+
+    if (isAuthPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL('/', req.url))
+      }
+      return null
+    }
+
+    if (!isAuth) {
+      return NextResponse.redirect(new URL('/login', req.url))
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token != null
+    },
+  }
+)
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
