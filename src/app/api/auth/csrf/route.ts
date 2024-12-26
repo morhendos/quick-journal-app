2f4d15e41c4d2461b7fce4e5b884d94370dc3939
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-26 20:20:20 +0200

    fix: Add CSRF route

--- src/app/api/auth/csrf/route.ts
+++ src/app/api/auth/csrf/route.ts
@@ -1,17 +1,3 @@
-import { type NextRequest } from 'next/server'
-import NextAuth from 'next-auth'
-import { authConfig } from '@/lib/auth/config'
+import { auth } from '@/lib/auth/config'
 
-const handler = NextAuth(authConfig)
-
-export async function GET(request: NextRequest) {
-  try {
-    const response = await handler(request)
-    return response
-  } catch (error) {
-    return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    })
-  }
-}
+export { auth as GET }
