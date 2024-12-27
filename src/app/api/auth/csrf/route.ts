441c7bd8e9b53f5ccd903c5779c33ffdf196688e
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-27 18:21:10 +0200

    fix: use NextAuth CSRF handler

--- src/app/api/auth/csrf/route.ts
+++ src/app/api/auth/csrf/route.ts
@@ -1,7 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getToken } from 'next-auth/jwt'
+import NextAuth from 'next-auth'
+import { authOptions } from '@/lib/auth'
 
-export async function GET(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
-  return NextResponse.json({ csrfToken: token?.['csrf-token'] ?? '' })
-}
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
