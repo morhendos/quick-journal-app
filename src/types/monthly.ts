8bccbd0f25147a20bd07c13dac831eabdf03c992
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-01-01 21:24:17 +0200

    fix: add missing monthly review item types

--- src/types/monthly.ts
+++ src/types/monthly.ts
@@ -8,3 +8,18 @@ export interface BaseItem {
 export interface WorkItem extends BaseItem {}
 export interface ProjectItem extends BaseItem {}
 export interface LearningItem extends BaseItem {}
+export interface HealthItem extends BaseItem {}
+export interface LifeEventItem extends BaseItem {}
+export interface LearningToRememberItem extends BaseItem {}
+export interface HopeItem extends BaseItem {}
+
+export interface MonthlyData {
+  month: string;
+  workItems: WorkItem[];
+  projectItems: ProjectItem[];
+  learningItems: LearningItem[];
+  healthItems: HealthItem[];
+  lifeEventItems: LifeEventItem[];
+  learningToRememberItems: LearningToRememberItem[];
+  hopeItems: HopeItem[];
+}
