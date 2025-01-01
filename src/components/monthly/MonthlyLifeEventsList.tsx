46e68959780a57f70b1a6e2e9b75014d2207167f
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-01-01 19:20:17 +0200

    Update list components with correct MonthlyList import

--- src/components/monthly/MonthlyLifeEventsList.tsx
+++ src/components/monthly/MonthlyLifeEventsList.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useMonthlyStorage } from '@/hooks/useMonthlyStorage';
 import { MonthlyList } from './MonthlyList';
+import { useMonthlyStorage } from '@/hooks/useMonthlyStorage';
 
 export function MonthlyLifeEventsList() {
   const {
